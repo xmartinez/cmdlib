a7ca25ff4cd39783da78526c164af0863b9d5f42
Xavier Martinez-Hidalgo <xavier@martinezhidalgo.org>
2020-06-15 19:24:41 +0200

    Bump version to 0.5.0

--- cmdlib.py
+++ cmdlib.py
@@ -8,7 +8,7 @@ from dataclasses import dataclass
 from textwrap import indent
 from typing import Any, List, Optional, Union
 
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 
 
 class CommandError(Exception):
