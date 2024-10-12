f7a0f61dfb14d5c4e606c76c6d29e00bd170c735
Xavier Martinez-Hidalgo <xavier@martinezhidalgo.org>
2024-10-12 19:55:08 +0200

    Bump poetry from 1.1.4 to 1.5.1 (#115)

--- scripts/install-poetry.sh
+++ scripts/install-poetry.sh
@@ -1,47 +1,25 @@
 #!/bin/bash
 set -euo pipefail
 
-http_fetch() {
-    local output="$1"; shift
-    local url="$1"; shift
-    echo "FETCH ${url} -> ${output}"
-    curl --fail --silent --show-error --globoff --location --output "${output}" "${url}"
+install_uv() {
+    local version="$1"; shift
+    local arch
+    arch=$(uname -m)
+
+    local basename=uv-${arch}-unknown-linux-gnu
+    local url=https://github.com/astral-sh/uv/releases/download/${version}/${basename}.tar.gz
+
+    mkdir -p ~/.local/bin
+    curl -sSfL "${url}" |
+        tar -O -xzf - "${basename}"/uv |
+        install /dev/stdin ~/.local/bin/uv
 }
 
 main() {
-    local root="$1"; shift
     local version="$1"; shift
-    local arch="$1"; shift
 
-    local tmp="/tmp/poetry.tar.gz"
-    local url="https://github.com/python-poetry/poetry/releases/download/${version}/poetry-${version}-${arch}.tar.gz"
-
-    install -v -d "${root}"
-
-    install -v -d "${root}/lib"
-    http_fetch "${tmp}" "${url}"
-    tar -C "${root}/lib" -xzf "${tmp}"
-
-    install -v -d "${root}/bin"
-    <<EOF install -T -m 755 /dev/stdin "${root}/bin/poetry"
-#!/usr/bin/env python3
-import sys
-import os.path
-
-lib = os.path.expanduser("${root}/lib")
-vendors = os.path.join(lib, "poetry", "_vendor")
-current_vendors = os.path.join(
-    vendors, "py{}".format(".".join(str(v) for v in sys.version_info[:2]))
-)
-
-sys.path.insert(0, lib)
-sys.path.insert(0, current_vendors)
-
-if __name__ == "__main__":
-    from poetry.console import main
-
-    main()
-EOF
+    install_uv 0.4.19
+    uv tool install poetry=="${version}"
 }
 
 main "$@"
